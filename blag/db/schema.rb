bb7b65ef18386edc6bc97c879de541ef3c509c50
Marshall Hattersley & Jacob Rogers <pair+mwhatters.shadylogic@devbootcamp.com>
2015-07-27 18:07:58 -0700

    ALL FEATURES IMPLEMENTED

--- blag/db/schema.rb
+++ blag/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150727231758) do
+ActiveRecord::Schema.define(version: 20150728002636) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,5 +23,15 @@ ActiveRecord::Schema.define(version: 20150727231758) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "comments", force: true do |t|
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "article_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["article_id"], name: "index_comments_on_article_id", using: :btree
 
 end
